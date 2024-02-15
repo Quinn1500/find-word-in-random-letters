0124e59a098edbc805bb3edeb9f773a84ef76623
Quinn1500 <130677747+Quinn1500@users.noreply.github.com>
2024-02-14 23:38:07 -0500

    Update with loading bar.py

--- with loading bar.py
+++ with loading bar.py
@@ -9,11 +9,11 @@ def main():
 
         loading_dots = ''
         while target_word not in generated_word:
-            generated_word += random.choice('abcdefghijklmnopqrstuvwxyz')
+            generated_word += generate_substring(target_word, len(generated_word))
             loading_dots = loading_animation(loading_dots)
             sys.stdout.write('\rGenerating word' + loading_dots)
             sys.stdout.flush()
-            time.sleep(0.5)
+            time.sleep(0.1)  # Adjusted sleep time
 
         print("\nTarget word '{}' generated!".format(target_word))
         print("It took {} letters to form the target word.".format(len(generated_word)))
@@ -21,6 +21,10 @@ def main():
         choice = input("Do you want to enter another word? (yes/no): ").lower()
         if choice != "yes":
             break
+
+def generate_substring(target_word, current_length):
+    # Generate a substring of the target word
+    return target_word[current_length:min(current_length + 10, len(target_word))]
 
 def loading_animation(dots):
     if len(dots) < 3:
